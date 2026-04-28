311b5afbcc0357ccc5ef2db10beaf22e63fb958b
cwchen.twn <cwchen.twn@gmail.com>
2026-04-28 11:53:06 -0300

    chore: Updated cloudflare mail dns for purelymail

--- email/main.tf
+++ email/main.tf
@@ -33,14 +33,19 @@ variable "domain" {
   description = "Domain name"
 }
 
-variable "dkim_selector" {
-  type = string
-  description = "DKIM selector"
-}
+# variable "dkim_selector" {
+#   type = string
+#   description = "DKIM selector"
+# }
 
-variable "dkim_pubkey" {
+# variable "dkim_pubkey" {
+#   type = string
+#   description = "DKIM public key"
+# }
+
+variable "purelymail_ownership_proof" {
   type = string
-  description = "DKIM public key"
+  description = "Purely Mail ownership proof token"
 }
 
 data "cloudflare_zone" "domain" {
@@ -49,63 +54,136 @@ data "cloudflare_zone" "domain" {
   }
 }
 
-// Polaris Mail Server Configuration
-// REF: https://wiki.polarismail.com/display/Support/Autodiscover
-resource "cloudflare_dns_record" "polarismail_autodiscover" {
+// Purely Mail Configuration
+// REF:
+// - https://purelymail.com/docs/domainDocs
+// - https://purelymail.com/manage/domain
+resource "cloudflare_dns_record" "purelymail_ownership" {
   zone_id = data.cloudflare_zone.domain.zone_id
-  comment = "Terraform created A record for Polaris Mail Autodiscover"
-  content = "69.28.212.195"
-  name = "autodiscover"
-  type = "A"
+  comment = "Terraform created SPF record for Purely Mail"
+  content = "\"purelymail_ownership_proof=${var.purelymail_ownership_proof}\""
+  name = "@"
+  type = "TXT"
+  ttl = 1
+}
+
+resource "cloudflare_dns_record" "purelymail_mx" {
+  zone_id = data.cloudflare_zone.domain.zone_id
+  comment = "Terraform created MX record for Purely Mail"
+  content = "mailserver.purelymail.com"
+  name = "@"
+  type = "MX"
+  ttl = 1 // 1 hour
+  priority = 50
+}
+
+resource "cloudflare_dns_record" "purelymail_spf" {
+  zone_id = data.cloudflare_zone.domain.zone_id
+  comment = "Terraform created SPF record for Purely Mail"
+  content = "\"v=spf1 include:_spf.purelymail.com ~all\""
+  name = "@"
+  type = "TXT"
+  ttl = 1
+}
+
+resource "cloudflare_dns_record" "purelymail_dkim_1" {
+  zone_id = data.cloudflare_zone.domain.zone_id
+  comment = "Terraform created DKIM record for Purely Mail 1"
+  name = "purelymail1._domainkey"
+  content = "key1.dkimroot.purelymail.com"
+  type = "CNAME"
   ttl = 1
   proxied = false
 }
 
-// REF: https://wiki.polarismail.com/display/Support/DNS+Configuration
-resource "cloudflare_dns_record" "polarismail_webmail" {
+resource "cloudflare_dns_record" "purelymail_dkim_2" {
   zone_id = data.cloudflare_zone.domain.zone_id
-  comment = "Terraform created CNAME record for Polaris Mail Webmail"
-  content = "webredirect.emailarray.com"
-  name = "webmail"
+  comment = "Terraform created DKIM record for Purely Mail 2"
+  name = "purelymail2._domainkey"
+  content = "key2.dkimroot.purelymail.com"
   type = "CNAME"
   ttl = 1
-  proxied = true
+  proxied = false
 }
 
-resource "cloudflare_dns_record" "polarismail_mx1" {
+resource "cloudflare_dns_record" "purelymail_dkim_3" {
   zone_id = data.cloudflare_zone.domain.zone_id
-  comment = "Terraform created MX record for Polaris Mail"
-  content = "mx.emailarray.com"
-  name = "@"
-  type = "MX"
-  ttl = 3600 // 1 hour
-  priority = 5
+  comment = "Terraform created DKIM record for Purely Mail 3"
+  name = "purelymail3._domainkey"
+  content = "key3.dkimroot.purelymail.com"
+  type = "CNAME"
+  ttl = 1
+  proxied = false
 }
 
-resource "cloudflare_dns_record" "polarismail_mx2" {
+resource "cloudflare_dns_record" "purelymail_dmark" {
   zone_id = data.cloudflare_zone.domain.zone_id
-  comment = "Terraform created MX record for Polaris Mail"
-  content = "mx2.emailarray.com"
-  name = "@"
-  type = "MX"
-  ttl = 3600 // 1 hour
-  priority = 10
+  comment = "Terraform created DMARC record for Purely Mail"
+  name = "_dmarc"
+  content = "dmarcroot.purelymail.com"
+  type = "CNAME"
+  ttl = 1
+  proxied = false
 }
 
-resource "cloudflare_dns_record" "polarismail_spf" {
-  zone_id = data.cloudflare_zone.domain.zone_id
-  comment = "Terraform created SPF record for Polaris Mail"
-  content = "\"v=spf1 include:emailarray.com -all\""
-  name = "@"
-  type = "TXT"
-  ttl = 1800 // 30 minutes
-}
 
-resource "cloudflare_dns_record" "polarismail_dkim" {
-  zone_id = data.cloudflare_zone.domain.zone_id
-  comment = "Terraform created DKIM record for Polaris Mail"
-  content = "\"${var.dkim_pubkey}\""
-  name = var.dkim_selector
-  type = "TXT"
-  ttl = 1 // automatic
-}
+// Polaris Mail Server Configuration
+// REF: https://wiki.polarismail.com/display/Support/Autodiscover
+# resource "cloudflare_dns_record" "polarismail_autodiscover" {
+#   zone_id = data.cloudflare_zone.domain.zone_id
+#   comment = "Terraform created A record for Polaris Mail Autodiscover"
+#   content = "69.28.212.195"
+#   name = "autodiscover"
+#   type = "A"
+#   ttl = 1
+#   proxied = false
+# }
+
+// REF: https://wiki.polarismail.com/display/Support/DNS+Configuration
+# resource "cloudflare_dns_record" "polarismail_webmail" {
+#   zone_id = data.cloudflare_zone.domain.zone_id
+#   comment = "Terraform created CNAME record for Polaris Mail Webmail"
+#   content = "webredirect.emailarray.com"
+#   name = "webmail"
+#   type = "CNAME"
+#   ttl = 1
+#   proxied = true
+# }
+
+# resource "cloudflare_dns_record" "polarismail_mx1" {
+#   zone_id = data.cloudflare_zone.domain.zone_id
+#   comment = "Terraform created MX record for Polaris Mail"
+#   content = "mx.emailarray.com"
+#   name = "@"
+#   type = "MX"
+#   ttl = 3600 // 1 hour
+#   priority = 5
+# }
+# 
+# resource "cloudflare_dns_record" "polarismail_mx2" {
+#   zone_id = data.cloudflare_zone.domain.zone_id
+#   comment = "Terraform created MX record for Polaris Mail"
+#   content = "mx2.emailarray.com"
+#   name = "@"
+#   type = "MX"
+#   ttl = 3600 // 1 hour
+#   priority = 10
+# }
+
+# resource "cloudflare_dns_record" "polarismail_spf" {
+#   zone_id = data.cloudflare_zone.domain.zone_id
+#   comment = "Terraform created SPF record for Polaris Mail"
+#   content = "\"v=spf1 include:emailarray.com -all\""
+#   name = "@"
+#   type = "TXT"
+#   ttl = 1800 // 30 minutes
+# }
+
+# resource "cloudflare_dns_record" "polarismail_dkim" {
+#   zone_id = data.cloudflare_zone.domain.zone_id
+#   comment = "Terraform created DKIM record for Polaris Mail"
+#   content = "\"${var.dkim_pubkey}\""
+#   name = var.dkim_selector
+#   type = "TXT"
+#   ttl = 1 // automatic
+# }
